b3ea7c137d6c85ebf6cf50558cf11c25f810fae2
Jackie Ma <97272592+majackie@users.noreply.github.com>
2023-12-27 21:18:10 -0800

    add desktop css

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-	var projects = document.querySelectorAll(".projects");
-	projects.forEach(function (project) {
-		var text = project.querySelector(".text");
-		var example = project.querySelector(".example");
-		var remainingHeight = project.clientHeight - text.clientHeight;
-		example.style.height = `${remainingHeight}px`;
-	});
+	if (window.innerWidth < 1024) {
+		var projects = document.querySelectorAll(".projects");
+		projects.forEach(function (project) {
+			var text = project.querySelector(".text");
+			var example = project.querySelector(".example");
+			var remainingHeight = project.clientHeight - text.clientHeight;
+			example.style.height = `${remainingHeight}px`;
+		});
+	}
 });
