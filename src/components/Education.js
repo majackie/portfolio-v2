96e61594628335965f9f2ec1694c3f3ac6dae21c
Jackie Ma <97272592+majackie@users.noreply.github.com>
2025-05-30 22:26:59 -0700

    complete education

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,14 +1,40 @@
 function Education() {
-    return (
-      <div id="education" className="mt-4">
-        <div className="text-2xl">Education</div>
-        <hr className="my-2" />
-        <div>
-
+  return (
+    <div id="education" className="mt-4">
+      <div className="text-2xl">Education</div>
+      <hr className="my-2" />
+      <div className="text-sm">
+        <div className="mt-4">
+          <div className="font-bold">Bachelor of Science in Applied Computer Science</div>
+          <div>British Columbia Institute of Technology</div>
+          <div className="flex justify-between text-sm">
+            <span>Burnaby, BC</span>
+            <span>Sep 2024 - May 2026</span>
+          </div>
+        </div>
+        <div className="mt-4">
+          <div className="font-bold">
+            Diploma in Computer Systems Technology
+          </div>
+          <div>British Columbia Institute of Technology</div>
+          <div className="flex justify-between text-sm">
+            <span>Burnaby, BC</span>
+            <span>Jan 2022 - Dec 2023</span>
+          </div>
+        </div>
+        <div className="mt-4">
+          <div className="font-bold">
+            Diploma in Architectural and Building Technology
+          </div>
+          <div>British Columbia Institute of Technology</div>
+          <div className="flex justify-between text-sm">
+            <span>Burnaby, BC</span>
+            <span>Sep 2017 - May 2020</span>
+          </div>
         </div>
       </div>
-    );
-  }
-  
-  export default Education;
-  
+    </div>
+  );
+}
+
+export default Education;
