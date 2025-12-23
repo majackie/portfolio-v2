24f1f48b815520c0b34db031d73ce7177e96f141
Jackie Ma <97272592+majackie@users.noreply.github.com>
2025-12-23 00:47:13 -0800

    update resume, distributedhashrecovery badges, gitignore

--- src/components/Projects/DistributedHashRecovery.js
+++ src/components/Projects/DistributedHashRecovery.js
@@ -1,9 +1,8 @@
 import Description from "./Description";
 import Demo from "./Demo";
 import Badge from "./Badge";
-import {
-  FaPython,
-} from "react-icons/fa";
+import { FaPython, FaNetworkWired, FaLock } from "react-icons/fa";
+import { SiSocketdotio } from "react-icons/si";
 import DemoVideo from "../../assets/videos/distributedhashrecovery.webm";
 
 function DistributedHashRecovery() {
@@ -14,12 +13,16 @@ function DistributedHashRecovery() {
         text="Recover passwords by distributing hash recovery tasks to multiple workers over a network. Supports generated hashes or extracted hashes from Linux shadow file."
         sourceCodeUrl="https://github.com/majackie/DistributedHashRecovery"
       />
-      <Demo
-        src={DemoVideo}
-        alt="DistributedHashRecovery Demo"
-      />
+      <Demo src={DemoVideo} alt="DistributedHashRecovery Demo" />
       <div className="flex flex-wrap gap-1 mt-2">
         <Badge icon={FaPython} label="Python" title="Python" />
+        <Badge icon={SiSocketdotio} label="Sockets" title="TCP/IP Sockets" />
+        <Badge
+          icon={FaNetworkWired}
+          label="Distributed Systems"
+          title="Distributed Systems"
+        />
+        <Badge icon={FaLock} label="Cryptography" title="Cryptography" />
       </div>
     </div>
   );
